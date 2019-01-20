50dc587a50f30550e28591c7ca0a2f0252faa609
s10724 <s10724@pjwstk.edu.pl>
2019-01-20 21:51:56 +0100

    Add report verb to neru

--- NERUtilitiesConsoleApp/Program.cs
+++ NERUtilitiesConsoleApp/Program.cs
@@ -34,6 +34,17 @@ namespace NERUtilitiesConsoleApp
             public bool GenerateReport { get; set; }
         }
 
+        [Verb("report", HelpText = "Test crf model from corpus text file")]
+        class ReportOptions
+        {
+            [Option('m', "model", Required = true, HelpText = "Computed model file.")]
+            public string ModelOutputFile { get; set; }
+            [Option('p', "pattern", Required = true, HelpText = "Test corpus file in text format.")]
+            public string CorpusTestFile { get; set; }
+            [Option('r', "report", Required = false, HelpText = "Generate report to file.")]
+            public bool GenerateReport { get; set; }
+        }
+
         [Verb("learn", HelpText = "Learn crf model from corpus text file")]
         class LearnOptions: LearnBase
         {
@@ -101,7 +112,23 @@ namespace NERUtilitiesConsoleApp
                     double correctNoEmptyPercent = Math.Round(((double)result.CorrectNotEmptyCount / ((double)result.CorrectNotEmptyCount + (double)result.IncorrectCount)) * 100.0, 2);
                     OutputText.WriteLine($"Results:\r\nCorrect: {result.CorrectCount}({correctPercent}%)\r\nCorrect not empty:{result.CorrectNotEmptyCount}({correctNoEmptyPercent}%)\r\nIncorrect: {result.IncorrectCount}");
                     OutputText.FileOutput = "";
-                });
+                })
+                .WithParsed<ReportOptions>(o =>
+                {
+                    if (o.GenerateReport)
+                    {
+                        DirectoryInfo di = Directory.CreateDirectory("reports");
+                        string fileReport = $@"reports/report_{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.txt";
+                        OutputText.WriteLine($"Save report to file: {fileReport}");
+                        OutputText.FileOutput = fileReport;
+                    }
+                    var crf = new CRFPP();
+                    var result = crf.RunTestAndCompareToCorpus(o.CorpusTestFile, o.ModelOutputFile);
+                    double correctPercent = Math.Round(((double)result.CorrectCount / ((double)result.CorrectCount + (double)result.IncorrectCount)) * 100.0, 2);
+                    double correctNoEmptyPercent = Math.Round(((double)result.CorrectNotEmptyCount / ((double)result.CorrectNotEmptyCount + (double)result.IncorrectCount)) * 100.0, 2);
+                    OutputText.WriteLine($"Results:\r\nCorrect: {result.CorrectCount}({correctPercent}%)\r\nCorrect not empty:{result.CorrectNotEmptyCount}({correctNoEmptyPercent}%)\r\nIncorrect: {result.IncorrectCount}");
+                    OutputText.FileOutput = "";
+                }); ;
 
 
 
